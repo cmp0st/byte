af6441b738e34f4454b631fae5b3d8ade22753c5
cmp0st <189583441+cmp0st@users.noreply.github.com>
2025-10-05 10:49:00 -0700

    fix: .gitnore only the golang binary
    Add ios code that was being blocked

---
+++ ios/Byte/Byte/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  Byte
+//
+//  Created by Nathan Smith on 10/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var appState = AppState()
+    
+    var body: some View {
+        Group {
+            if appState.isConfigured {
+                MainTabView()
+            } else {
+                SetupView()
+            }
+        }
+        .environmentObject(appState)
+    }
+}
+
+#Preview {
+    ContentView()
+}
